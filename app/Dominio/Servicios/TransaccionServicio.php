8d856412331bb6e95ed8e305858edaaf7a87b77a
fxreverLil <projasma2002@gmail.com>
2024-04-20 10:55:29 -0500

    Transferencia validando el servicio de autorizacion externa

--- app/Dominio/Servicios/TransaccionServicio.php
+++ app/Dominio/Servicios/TransaccionServicio.php
@@ -7,19 +7,23 @@
         }
 
         public function solicitarTransferencia($monto,$origen,$destino){
-            $user = $this->repositorio->buscarUsuario($origen);
-            if($user instanceof Usuario){
-                if($user->getTipo() != 'comerciante'){
-                    if($this->verificarSaldo($user,$monto)){
-                        return $this->repositorio->transferir($monto,$origen,$destino);
+            if($this->servicioExterno()){
+                $user = $this->repositorio->buscarUsuario($origen);
+                if($user instanceof Usuario){
+                    if($user->getTipo() != 'comerciante'){
+                        if($this->verificarSaldo($user,$monto)){
+                            return $this->repositorio->transferir($monto,$origen,$destino);
+                        } else {
+                            return 'Saldo insuficiente';
+                        }
                     } else {
-                        return 'Saldo insuficiente';
+                        return 'El tipo comerciante no puede realizar transferencias';
                     }
-                } else {
-                    return 'El tipo comerciante no puede realizar transferencias';
+                } else{
+                    return 'No se encontro al usuario';
                 }
-            } else{
-                return 'No se encontro al usuario';
+            } else {
+                return 'Error en autorizacion del servicio de transferencia';
             }
         }
 
@@ -28,6 +32,19 @@
                 return true;
             }
         }
+
+        public function servicioExterno() {
+            $url = 'https://run.mocky.io/v3/1f94933c-353c-4ad1-a6a5-a1a5ce2a7abe';
+            $response = file_get_contents($url);
+            $data = json_decode($response, true);
+        
+            if (isset($data['message']) && $data['message'] == 'Autorizado') {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        
 
     }
 ?>
